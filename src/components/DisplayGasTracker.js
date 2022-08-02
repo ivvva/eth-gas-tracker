c6a40a134570e587fa0aa370f90571e8a3d7cb11
iva <ivvvpol@gmail.com>
2022-08-02 17:59:47 +0200

    display data

---
+++ src/components/DisplayGasTracker.js
@@ -0,0 +1,24 @@
+import React from "react";
+import axios from "axios";
+
+export default function DisplayGasTracker(props) {
+  console.log(props.data)
+  // const displayData = (props) => {
+  //   const { data } = props;
+
+  //   if (data.length > 0){
+  //     return(
+  //       data.map((data, i) => {
+  //         console.log(data);
+  //         return(
+  //           <div>hi</div>
+  //         )
+  //       }) 
+  //     )
+  //   } else {
+  //     return (<div>Bru</div>)
+  //   }
+  // };
+
+  return <div></div>;
+}
--- src/components/GasTracker.js
+++ src/components/GasTracker.js
@@ -1,20 +1,32 @@
 import React, { useEffect, useState } from "react";
+import DisplayGasTracker from "./DisplayGasTracker";
 import axios from "axios";
 
 export default function GasTracker() {
-  const [data, setData] = useState([]);
-
-  const getData = async () => {
-    const { data } = await axios.get(
-      `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${process.env.REACT_APP_ETH_SCAN_API_KEY} `
-    );
-    setData(data);
-    console.log(data)
-  };
+  const [data, setData] = useState("");
 
   useEffect(() => {
-    getData();
+    getAllData();
   }, []);
 
-  return <div>GasTracker</div>;
+  const getAllData = async () => {
+    axios
+      .get(
+        `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${process.env.REACT_APP_ETH_SCAN_API_KEY}`
+      )
+      .then((res) => {
+        const allData = res.data;
+        console.log(allData)
+        setData(allData);
+      })
+      .catch((err) => console.error(`Error: ${err}`));
+    //   const { data } = await axios.get(
+    //     `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${process.env.REACT_APP_ETH_SCAN_API_KEY} `
+    //   );
+    //   console.log({bru: data})
+    //  ;
+    //   console.log(data.result.ProposeGasPrice)
+  };
+
+  return <DisplayGasTracker data={data} />;
 }
