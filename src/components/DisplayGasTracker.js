3c814246c9dcb954ccb745fbb0e56530f6c32112
iva <ivvvpol@gmail.com>
2022-08-06 12:12:54 +0200

    prettier

--- src/components/DisplayGasTracker.js
+++ src/components/DisplayGasTracker.js
@@ -9,21 +9,21 @@ export default function DisplayGasTracker(props) {
     if (data) {
       return (
         <>
-        <div  className="logo"><img src={logo} /> <h1>Gas Tracker</h1></div>
-        <div className="bod">
-          <div className="container">
-            <div className="prices">
-              <div className="price">Low  😄 <br></br>{data.SafeGasPrice} <p>GWEI</p></div>
-              <div className="price">Average  🙂 <br></br>{data.ProposeGasPrice}<p>GWEI</p> </div>
-              <div className="price">High  🧐 <br></br> {data.FastGasPrice}<p>GWEI</p> </div>
+          <div className="logo"><img src={logo} /> <h1>Gas Tracker</h1></div>
+          <div className="bod">
+            <div className="container">
+              <div className="prices">
+                <div className="price">Low  😄 <br></br>{data.SafeGasPrice} <p>GWEI</p></div>
+                <div className="price">Average  🙂 <br></br>{data.ProposeGasPrice}<p>GWEI</p> </div>
+                <div className="price">High  🧐 <br></br> {data.FastGasPrice}<p>GWEI</p> </div>
+              </div>
+              <div className="lastBlock">Last Block: <a href={`https://etherscan.io/block/${data.LastBlock}`}>{data.LastBlock}</a></div>
             </div>
-            <div className="lastBlock">Last Block: <a href={`https://etherscan.io/block/${data.LastBlock}`}>{data.LastBlock}</a></div>
+            <div className="circles">
+              <div className="circle circle-1"></div>
+              <div className="circle circle-2"></div>
+            </div>
           </div>
-          <div className="circles">
-            <div className="circle circle-1"></div>
-            <div className="circle circle-2"></div>
-          </div>
-        </div>
         </>
       );
     } else return <div>No data here!</div>;
